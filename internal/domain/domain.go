0140550bd3df39214b5a37a16077a472398d18c5
unk_xui <xbydji@gmail.com>
2022-11-01 21:50:11 +0400

    add tests

--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -1,5 +1,12 @@
 package domain
 
+import (
+	"fmt"
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+	"regexp"
+)
+
 type Config struct {
 	Port string
 	IP   string
@@ -10,9 +17,49 @@ type Config struct {
 	DbPort     string
 }
 
+type UserInfoPhone struct {
+	Phone string `json:"phone"`
+}
+
+func (ths *UserInfoPhone) Validation() error {
+	return validation.ValidateStruct(
+		ths,
+		validation.Field(&ths.Phone, validation.Required, validation.Length(11, 11), is.Digit),
+	)
+}
+
 type UserInfo struct {
 	ID       uint32 `json:"user_id"`
 	Name     string `json:"name"`
 	Password string `json:"password"`
 	Phone    string `json:"phone"`
+}
+
+func (ths *UserInfo) Validate() error {
+	return validation.ValidateStruct(
+		ths,
+		validation.Field(&ths.Name, is.ASCII),
+		validation.Field(&ths.Password, validation.Match(regexp.MustCompile(`[a-zA-Z0-9]]`))),
+	)
+}
+
+type Response struct {
+	IsPhone bool        `json:"is_phone"`
+	Data    interface{} `json:"data"`
+}
+
+func (ths *Response) Parse() error {
+	if ths.IsPhone {
+		phoneStruct := ths.Data.(UserInfoPhone)
+
+		fmt.Println(phoneStruct)
+
+		return nil
+	} else {
+		Struct := ths.Data.(UserInfo)
+
+		fmt.Println(Struct)
+
+		return nil
+	}
 }
